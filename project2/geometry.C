e6d54291546f4bbb8ef8e9a2fcec6cf2061c5908
zjpierson <zachary.pierson@mines.sdsmt.edu>
2014-12-05 21:06:41 -0700

    found minMax x and y values

--- project2/geometry.C
+++ project2/geometry.C
@@ -4,6 +4,7 @@
 //                                                                //
 ////////////////////////////////////////////////////////////////////
 
+#include <string>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
@@ -16,39 +17,49 @@ using namespace std;
 ////////////////////////////////////////////////////////////////////
 
 struct point
-  {
-  double x;
-  double y;
-  };
+{
+    double x;
+    double y;
+};
+
+struct minMax
+{
+    double minx = 999;
+    double miny = 999;
+    double maxx = -999;
+    double maxy = -999;
+};
+
+
 
 ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////
 
 double min(double a, double b)
-  {
-  if (a < b)
-    return a;
-  return b;
-  }
+{
+    if (a < b)
+        return a;
+    return b;
+}
 
 ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////
 
 double max(double a, double b)
-  {
-  if (a > b)
-    return a;
-  return b;
-  }
+{
+    if (a > b)
+        return a;
+    return b;
+}
 
 ////////////////////////////////////////////////////////////////////
 //          Cross product of vectors a and b                      //
 ////////////////////////////////////////////////////////////////////
 
 double cross(point a, point b)
-  {
-  return a.x * b.y - a.y * b.x;
-  }
+{
+    return a.x * b.y - a.y * b.x;
+}
 
 ////////////////////////////////////////////////////////////////////
 //      Is c on the line segment from a to b?                     //
@@ -56,13 +67,13 @@ double cross(point a, point b)
 ////////////////////////////////////////////////////////////////////
 
 bool on(point a, point b, point c)
-  {
-  if (((min(a.x, b.x) <= c.x) && (c.x <= max(a.x, b.x))) &&
-      ((min(a.y, b.y) <= c.y) && (c.y <= max(a.y, b.y))))
-    return true;
-     
-  return false;
-  }
+{
+    if (((min(a.x, b.x) <= c.x) && (c.x <= max(a.x, b.x))) &&
+            ((min(a.y, b.y) <= c.y) && (c.y <= max(a.y, b.y))))
+        return true;
+
+    return false;
+}
 
 ////////////////////////////////////////////////////////////////////
 //       Compute cross product of vector from a to b and          //
@@ -71,38 +82,38 @@ bool on(point a, point b, point c)
 ////////////////////////////////////////////////////////////////////
 
 double direction(point a, point b, point c)
-  {
-  point ab;
-  point bc;
-  double result;
+{
+    point ab;
+    point bc;
+    double result;
 
-  ab.x = b.x - a.x;
-  ab.y = b.y - a.y;
-  bc.x = c.x - b.x;
-  bc.y = c.y - b.y;
-  result =  cross(ab, bc);
-  if (fabs(result) < 1.0e-6)
-    result = 0.0;
-  return result;
-  }
+    ab.x = b.x - a.x;
+    ab.y = b.y - a.y;
+    bc.x = c.x - b.x;
+    bc.y = c.y - b.y;
+    result =  cross(ab, bc);
+    if (fabs(result) < 1.0e-6)
+        result = 0.0;
+    return result;
+}
 
 ////////////////////////////////////////////////////////////////////
 //    Is the turn from a to b to c clockwise?                     //
 ////////////////////////////////////////////////////////////////////
 
 bool turn(point a, point b, point c)
-  {
-  return (direction(a, b, c) > 0);
-  }
+{
+    return (direction(a, b, c) > 0);
+}
 
 ////////////////////////////////////////////////////////////////////
 //    Is the turn from p[0] to p[1] to p[2] clockwise?            //
 ////////////////////////////////////////////////////////////////////
 
 bool clockwise(point p[])
-  {
-  return turn(p[0], p[1], p[2]);
-  }
+{
+    return turn(p[0], p[1], p[2]);
+}
 
 ////////////////////////////////////////////////////////////////////
 //     From text, page 937 (second edition)                       //
@@ -111,55 +122,55 @@ bool clockwise(point p[])
 ////////////////////////////////////////////////////////////////////
 
 bool intersect(point p1, point p2, point p3, point p4)
-  {
-  double d1;
-  double d2;
-  double d3;
-  double d4;
+{
+    double d1;
+    double d2;
+    double d3;
+    double d4;
 
-  d1 = direction(p3, p4, p1);
-  d2 = direction(p3, p4, p2);
-  d3 = direction(p1, p2, p3);
-  d4 = direction(p1, p2, p4);
+    d1 = direction(p3, p4, p1);
+    d2 = direction(p3, p4, p2);
+    d3 = direction(p1, p2, p3);
+    d4 = direction(p1, p2, p4);
 
-  if ((((d1 > 0) && (d2 < 0)) || ((d1 < 0) && (d2 > 0))) &&
-      (((d3 > 0) && (d4 < 0)) || ((d3 < 0) && (d4 > 0))))
-    return true;
+    if ((((d1 > 0) && (d2 < 0)) || ((d1 < 0) && (d2 > 0))) &&
+            (((d3 > 0) && (d4 < 0)) || ((d3 < 0) && (d4 > 0))))
+        return true;
 
-  if ((d1 == 0) && on(p3, p4, p1))
-    return true;
+    if ((d1 == 0) && on(p3, p4, p1))
+        return true;
 
-  if ((d2 == 0) && on(p3, p4, p2))
-    return true;
+    if ((d2 == 0) && on(p3, p4, p2))
+        return true;
 
-  if ((d3 == 0) && on(p1, p2, p3))
-    return true;
+    if ((d3 == 0) && on(p1, p2, p3))
+        return true;
 
-  if ((d4 == 0) && on(p1, p2, p4))
-    return true;
+    if ((d4 == 0) && on(p1, p2, p4))
+        return true;
 
-  return false;
-  }
+    return false;
+}
 
 ////////////////////////////////////////////////////////////////////
 //          Area of polygon by adding/subtracting trapezoids      //
 ////////////////////////////////////////////////////////////////////
 
 double area(point p[], int n)
-  {
-  int i;
-  int j;
-  double result;
+{
+    int i;
+    int j;
+    double result;
 
-  result = 0;
-  for (i = 0; i < n; i++)
+    result = 0;
+    for (i = 0; i < n; i++)
     {
-    j = (i + 1) % n;
-    result += p[i].x * p[j].y;
-    result -= p[i].y * p[j].x;
+        j = (i + 1) % n;
+        result += p[i].x * p[j].y;
+        result -= p[i].y * p[j].x;
     }
-  return fabs(result / 2);
-  }
+    return fabs(result / 2);
+}
 
 
 void check_args(int argc)
@@ -173,16 +184,25 @@ void check_args(int argc)
 
 void open_files(ifstream &fin, ofstream &fout, char **argv)
 {
+    string infile = argv[1];
+    string outfile = argv[2];
+    
+
     //Open and check if input file exists
-    fin.open(argv[1], ios::in);
+    fin.open(infile.append(".in").c_str(), ios::in);
     if(!fin)
     {
         cout << "File " << argv[1] << "could not be opened" << endl;
         exit (1);
     }
 
+
+
+
+    outfile.append(".out");
+
     //Open and check if output file exists
-    fout.open(argv[2], ios::out);
+    fout.open(outfile.c_str(), ios::out);
     if(!fout)
     {
         cout << "File " << argv[2] << "could not be opened" << endl;
@@ -190,7 +210,7 @@ void open_files(ifstream &fin, ofstream &fout, char **argv)
     }
 }
 
-void get_coords(ifstream &fin, vector<point> &input)
+void get_coords(ifstream &fin, vector<point> &input, minMax &boundry)
 {
     point newPoint;
     double c;
@@ -203,6 +223,15 @@ void get_coords(ifstream &fin, vector<point> &input)
         newPoint.y = c;
 
         input.push_back(newPoint);
+
+        if(newPoint.x < boundry.minx)
+            boundry.minx = newPoint.x;
+        if(newPoint.y < boundry.miny)
+            boundry.miny = newPoint.y;
+        if(newPoint.x > boundry.maxx)
+            boundry.maxx = newPoint.x;
+        if(newPoint.y > boundry.maxy)
+            boundry.maxy = newPoint.y;
     }
 }
 
@@ -221,34 +250,23 @@ int main(int argc, char **argv)
     //Open and check input and output files
     open_files(fin, fout, argv);
 
+    minMax boundry;
+
     //Fill vector with points from input file
     vector<point> input;
-    get_coords(fin, input);
-    
-//    for(int i = 0; i < input.size() ; i++)
-//        cout << input[i].x << " " << input[i].y << endl;
+    get_coords(fin, input, boundry);
+
+
+    for(unsigned int i = 0; i < input.size() ; i++)
+        cout << input[i].x << " " << input[i].y << endl;
+
+    cout << "boundry.maxx = " << boundry.maxx << endl;
+    cout << "boundry.maxy = " << boundry.maxy << endl;
+    cout << "boundry.minx = " << boundry.minx << endl;
+    cout << "boundry.miny = " << boundry.miny << endl;
 
 
 
-    //Check to see if input file extension is .in
-//    char *c = strchr(argv[1], '.');
-//    if(  c != NULL)
-//        strcat(c, ".in\0");
-//    else
-//        strcat(argv[1], ".in\0");
-//
-//    cout << argv[1] << endl;
-
-    //Check to see if output file extension is .out
-//    if(!argv[2].find("."))
-//        argv[2].append(".out");
-//    else
-//        argv[2].append(".out
-//
-//    vector<point> input; 
-//    ifstream fin;
-//    fin.open(argv[1]);
-//    //read input file into array of points
 
 
     fin.close();
